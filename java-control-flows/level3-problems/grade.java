cd723dcde2a6c36b78f148f74f41723d6b8beb08
Git466-stack <makursubham.23@gmail.com>
2025-02-17 11:03:21 +0530

    commit

--- java-control-flows/level3-problems/grade.java
+++ java-control-flows/level3-problems/grade.java
@@ -9,10 +9,39 @@ public class grade {
 
         int chemistryMarks = sc.nextInt();
 
-        int mathMarks = sc.nextInt()
-
-
+        int mathMarks = sc.nextInt();
         
+
+        double percentage = physicsMarks+chemistryMarks+mathMarks/3.0;
+
+        String remarks,  grade;
+        if(percentage>=80){
+            grade = "A";
+            remarks = "(Level 4, above agency-normalized standards)";
+        }
+        else if(percentage >=70 && percentage<=79){
+            grade = "B";
+            remarks = "(Level 3, at agency- normalized standars)";
+        }
+        else if(percentage>=60 && percentage<=69){
+            grade="C";
+            remarks = "(Level 2, below , but approaching agency- normalized standards)";
+        }
+        else if(percentage>=50 && percentage<=59){
+            grade = "D";
+            remarks = "(Level 1 , well below agency-normalized standards)";
+        }
+        else if(percentage>=40 && percentage<=49){
+            grade = "E";
+            remarks = "(Level 1-, too below agency-normalized standards)";
+        }
+        else{
+            grade = "R";
+            remarks = "(Remedial standard)";
+        }
+        System.out.println("Average Percentage: " + percentage + "%"); 
+        System.out.println("Grade: " + grade);
+        System.out.println("Remarks: " + remarks);
 
     }
 }
