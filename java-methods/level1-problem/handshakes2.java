ca5ffd2e07dee8d59d82433c3df20e895e93ca6c
Git466-stack <makursubham.23@gmail.com>
2025-02-20 09:55:00 +0530

    commit

--- java-methods/level1-problem/handshakes2.java
+++ java-methods/level1-problem/handshakes2.java
@@ -18,31 +18,3 @@ public class handshakes2 {
 }
 
 
-class Car {
-    String name;
-    int year;
-    String color;
-    
-    Car(String name, int year, String color) {
-        this.name = name;
-        this.year = year;
-        this.color = color;
-    }
-    
-    // display method
-    void display() {
-        System.out.println("Car details: Name = " + name + ", Year = " + year + ", Color = " + color);
-    }
-}
-
-class Main {
-    public static void main(String[] args) {
-        // Create objects for the Car class
-        Car car1 = new Car("maruti", 2003, "black");
-        Car car2 = new Car("thar", 1995, "black");
-        
-        // Call the display method
-        car1.display();
-        car2.display();
-    }
-}
