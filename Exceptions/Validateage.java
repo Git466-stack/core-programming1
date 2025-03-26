d2dd39693958211588704656e836e8ef7f552f04
Git466-stack <makursubham.23@gmail.com>
2025-03-27 00:17:31 +0530

    [Subham]fix: sc.close fix

---
+++ Exceptions/Validateage.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+class InvalidAgeException extends Exception {
+    InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class Validateage {
+
+    // Handle custom exception
+    public static void validateAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age is not valid");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter the age");
+            int age = sc.nextInt();
+            validateAge(age);
+
+            System.out.println("Access Granted");
+
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+        sc.close();
+
+    }
+}
